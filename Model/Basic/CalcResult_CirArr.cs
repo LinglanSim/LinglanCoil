fde0d4ea51d9d3f5da66278bef100cdadc89a9e5
jm.liu <jingming.liu@midea.com>
2018-02-01 09:01:31 +0800

    Circuit-Reverse and AutoCircuit modules

---
+++ Model/Basic/CalcResult_CirArr.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Basic
+{
+    public class CalcResult_CirArr
+    {
+        public int[,] CirArrange = new int[100 , 100];
+        public int AddNumber_1;
+        public int AddNumber_2;
+        public int AddNumber_3;
+
+
+        public CalcResult ShallowCopy()
+        {
+            return (CalcResult)this.MemberwiseClone();
+        }
+    }
+}
--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -12,8 +12,7 @@ namespace Test
         {
             //var r = Main.main_evaporator();
             //var r1 = Main.main_condenser();
-            //var r = Main.Water_Midea5();
-            //var r = Main.Water_Midea5_reverse();
+            //var r = Main.Water_Midae5();
             //var rr = Main.Water_Midea9();
             //var rrr = Main.Water_Cool_Midea9();
             //var rr = Main.Water_Heat_Midea9();
@@ -21,15 +20,19 @@ namespace Test
             //var rr = Main.Water_Cool_Jiayong6();
             //var rr = Main.Water_Heat_Jiayong2();
             //var rr = Main.Water_Cool_Jiayong2();
-            //var rr = Main.Water_Heat_Jiayong6_autosplitCir();
-            //var rr = Main.Water_Heat_Jiayong2_autosplitCir();
-            var rr = Main.Water_Heat_Zhongyang1();
+            //var rr = Main.Water_Heat_Jiayong6_reverse();
+            //var rr = Main.Water_Heat_Jiayong2_reverse();
+            //var rr = Main.Water_Midea9_reverse();
+            //var rr = Main.Water_Heat_Zhongyang1_reverse();
+            //var rr = Main.Water_Heat_Jiayong6_autosplitCir_New();
+            var rr = Main.Water_Heat_Jiayong2_autosplitCir();
+            //var rr = Main.Water_Heat_Zhongyang1_autosplitCir();
+            //var rr = Main.Water_Heat_Zhongyang1();
             //var rr = Main.Water_Cool_Zhongyang1();
             //var rr = Main.Water_Heat_Zhongyang2();
             //var rr = Main.Water_Cool_Zhongyang2();
             //var r = Main.MinNout();
             //var r = Main.NinMout();
-
         }
     }
 }
