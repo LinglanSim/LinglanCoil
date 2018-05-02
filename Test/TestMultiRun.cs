cc36a2a783ed2625692bd98d3d9ca3667df1cc4f
ourh <ozihao@hotmail.com>
2018-05-02 11:28:13 +0800

    Add Simulation time for MultiRun Test

--- Test/TestMultiRun.cs
+++ Test/TestMultiRun.cs
@@ -37,8 +37,7 @@ namespace Test
                 airInput.RHi = 0.5;
                 airInput.ha = 80;
 
-                //geo input
-                //mm
+                //geo input, mm
                 geoInput.Pt = 21;
                 geoInput.Pr = 22;
                 geoInput.Di = 6.9;
@@ -49,10 +48,13 @@ namespace Test
                 geoInput.Nrow = 2;
                 geoInput.Ntube = 24;
                 geoInput.CirNum = 2;
+                DateTime Time1 = DateTime.Now;
                 var rr = Main.main_condenser_py(refInput, airInput, geoInput);
+                DateTime Time2 = DateTime.Now;
+                double time = (Time2 - Time1).TotalSeconds;
                 using (StreamWriter wr = File.AppendText(@"D:\Work\Simulation\Test\smoothness.txt"))
                 {
-                    wr.WriteLine("v, {0}, href, {1}, PressD, {2}, Q, {3}, Xo, {4}, Tro, {5}, Hro, {6}", airInput.Volumetricflowrate, rr.href, rr.DP, rr.Q, rr.x_o, rr.Tro, rr.hro);
+                    wr.WriteLine("v, {0}, href, {1}, PressD, {2}, Q, {3}, Xo, {4}, Tro, {5}, Hro, {6}, Charge, {7}, Time, {8}", airInput.Volumetricflowrate, rr.href, rr.DP, rr.Q, rr.x_o, rr.Tro, rr.hro, rr.M, time);
                 }
             }
 
