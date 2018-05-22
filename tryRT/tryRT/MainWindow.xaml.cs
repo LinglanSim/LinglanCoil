7e3801d455b078019a2636dfa5a6653031d8f79b
jm.liu <jingming.liu@midea.com>
2018-05-22 16:35:28 +0800

    No.5 Add user-defined

--- tryRT/tryRT/MainWindow.xaml.cs
+++ tryRT/tryRT/MainWindow.xaml.cs
@@ -43,15 +43,17 @@ namespace tryRT
         public void Bind1()
         {
             IList<string> customList = new List<string>();
-            string[] sss = new string[3];
+            string[] sss = new string[4];
             //if curve = 1, geometry parameter is:Do:5mm,Pt:14.5mm,Pl:12.56mm,Fin_type:plain,Tf:0.095,Pf:1.2mm;
             //if curve = 4, geometry parameter is:Do:8mm,Pt:22mm,Pl:19.05mm,Fin_type:plain,Tf:0.1,Pf:1.6mm;
             sss[0] = "Do=7mm,Row=2,Pt=22mm,Pr=21mm";
             sss[1] = "Do=5mm,Row=2,Pt=14.5mm,Pr=12.56mm";
             sss[2] = "Do=8mm,Row=2,Pt=22mm,Pr=19.05mm";
+            sss[3] = "User Defined";
             customList.Add(sss[0]);
             customList.Add(sss[1]);
             customList.Add(sss[2]);
+            customList.Add(sss[3]);
 
             ComboBox1.ItemsSource = customList;
             ComboBox1.SelectedValue = customList[0];
@@ -220,16 +222,26 @@ namespace tryRT
 
         private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
-            string[] sp = ComboBox1.SelectedItem.ToString().Split(new string[] { "," }, StringSplitOptions.None);
+            if (ComboBox1.SelectedItem.ToString() == "User Defined")
+            {
+                Pt.IsEnabled = true;
+                Pr.IsEnabled = true;
+                Row.IsEnabled = true;
+                Do.IsEnabled = true;
+            }
+            else
+            {
+                string[] sp = ComboBox1.SelectedItem.ToString().Split(new string[] { "," }, StringSplitOptions.None);
 
-            string sp0 = sp[0].Split(new string[] { "=" }, StringSplitOptions.None).Last();
-            Do.Text = sp0.Remove((sp0.Length - 2), 2);
-            string sp1 = sp[1].Split(new string[] { "=" }, StringSplitOptions.None).Last();
-            Row.Text = sp1;// sp1.Remove((sp1.Length - 2), 2);
-            string sp2 = sp[2].Split(new string[] { "=" }, StringSplitOptions.None).Last();
-            Pt.Text = sp2.Remove((sp2.Length - 2), 2);
-            string sp3 = sp[3].Split(new string[] { "=" }, StringSplitOptions.None).Last();
-            Pr.Text = sp3.Remove((sp3.Length - 2), 2);
+                string sp0 = sp[0].Split(new string[] { "=" }, StringSplitOptions.None).Last();
+                Do.Text = sp0.Remove((sp0.Length - 2), 2);
+                string sp1 = sp[1].Split(new string[] { "=" }, StringSplitOptions.None).Last();
+                Row.Text = sp1;// sp1.Remove((sp1.Length - 2), 2);
+                string sp2 = sp[2].Split(new string[] { "=" }, StringSplitOptions.None).Last();
+                Pt.Text = sp2.Remove((sp2.Length - 2), 2);
+                string sp3 = sp[3].Split(new string[] { "=" }, StringSplitOptions.None).Last();
+                Pr.Text = sp3.Remove((sp3.Length - 2), 2);
+            }   
         }
 
         private void ComboBox_SelectionChanged_2(object sender, SelectionChangedEventArgs e)
